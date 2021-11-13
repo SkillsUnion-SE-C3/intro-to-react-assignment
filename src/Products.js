baebb292917baa89169e14c6929cf22ae896fd4a
Andrew Ralph-Gledhill <marleyspark@hotmail.com>
2021-11-13 13:03:08 +0000

    Add Assignment

--- src/Products.js
+++ src/Products.js
@@ -1,1 +1,15 @@
 import products from "./productList";
+
+function Products() {
+    function logProducts(name) {
+        console.log(name)
+    }
+
+    return (
+        <div className="container">{products.map((item, index) => {
+            return <h2 key={index} onClick={() => logProducts(item.name)}>{item.name} {item.price}</h2>
+        })}</div>
+    )
+}
+
+export default Products
