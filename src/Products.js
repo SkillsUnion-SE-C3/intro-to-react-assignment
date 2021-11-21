24bb31a570af3c496a19aac4be65ded689f70382
Aishahassan1 <aishahassan_5@hotmail.com>
2021-11-21 21:49:12 +0000

    add code

--- src/Products.js
+++ src/Products.js
@@ -1,1 +1,16 @@
 import products from "./productList";
+
+function Products() {
+    return (
+    <div className="container">
+        <h2>Product List</h2>
+        {products.map ((product) => {
+            return(
+                <p onClick={()=> console.log(product.name)}>{product.name} {product.price}</p>
+            )
+        })}
+    </div>
+    )
+}
+
+export default Products
